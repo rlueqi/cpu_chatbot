7190e6874d8192a5cbe972c32de5fb11c9990791
rlueqi <rlueqi@gmail.com>
2025-12-02 02:59:31 +0000

    st.title 변경

--- main.py
+++ main.py
@@ -49,7 +49,7 @@ llm_model = "gpt-oss-120b"
 if "llm_model" not in st.session_state:
     st.session_state["llm_model"] = llm_model
 
-st.title("나의 AI 친구")
+st.title("언어 습득용 특화 AI")
 
 # 시스템 메시지 설정
 if "messages" not in st.session_state:
